02195793930a310e143b4184e1496922a8281997
oktest145 <83488344+oktest145@users.noreply.github.com>
2021-09-23 21:09:23 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -22,8 +22,8 @@ from logger import LOGGER
 
 buttons = [
     [
-        InlineKeyboardButton('⚡️Make Own Bot', url='https://github.com/subinps/VCPlayerBot'),
-        InlineKeyboardButton('🧩 Join Here', url='https://t.me/subin_works'),
+        InlineKeyboardButton('⚡️Make Own Bot', url='https://github.com/oktest145/VCPlayerBot-1'),
+        InlineKeyboardButton('🧩 Join Here', url='https://t.me/ironman_cloud'),
     ]
     ]
 def get_cmd(dur):
